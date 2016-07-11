11e39d2e5e7d438880c7b4af48c3df5730b0b642
Aaron Fields <afields410@gmail.com>
2016-07-11 11:47:50 -0500

    added actors

--- Actors_List_Trial1/app/src/main/java/ly/generalassemb/actors_list_trial1/MainActivity.java
+++ Actors_List_Trial1/app/src/main/java/ly/generalassemb/actors_list_trial1/MainActivity.java
@@ -3,11 +3,29 @@ package ly.generalassemb.actors_list_trial1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    ArrayList<String> mActorsList;
+    String actor1;
+    String actor2;
+    String actor3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        actor1 = "Christopher Waltz";
+        actor2 = "Steve Carrell";
+        actor3 = "BJ Novak";
+
+        mActorsList =new ArrayList<String>();
+
+        mActorsList.add(actor1);
+        mActorsList.add(actor2);
+        mActorsList.add(actor3);
+
+
     }
 }
