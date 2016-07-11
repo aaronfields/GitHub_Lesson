0e56201280763153b02861c16717a62593c5c719
Aaron Fields <afields410@gmail.com>
2016-07-11 10:39:39 -0500

    first commit

---
+++ Actors_List_Trial1/app/src/main/java/ly/generalassemb/actors_list_trial1/MainActivity.java
@@ -0,0 +1,13 @@
+package ly.generalassemb.actors_list_trial1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
