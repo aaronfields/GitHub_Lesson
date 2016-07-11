c056098fa61e5699e1e7263c2a59ec18a6a70b67
bphillips91 <Brendan.Phillips91@Gmail.com>
2016-07-11 11:49:39 -0500

    MainActivity

--- Actors_List_Trial1/app/src/main/java/ly/generalassemb/actors_list_trial1/MainActivity.java
+++ Actors_List_Trial1/app/src/main/java/ly/generalassemb/actors_list_trial1/MainActivity.java
@@ -2,12 +2,35 @@ package ly.generalassemb.actors_list_trial1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    RecyclerAdapter mRecyclerAdapter;
+    RecyclerView.LayoutManager layoutManager;
+    RecyclerView mRecyclerView;
+    List<Actor> mActorList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+
+        Actor johnCena = new Actor("John Cena",39,"Male");
+        Actor chloeBennet = new Actor("Chloe Bennet",24,"Female");
+        Actor sethRogen = new Actor ("Seth Rogen",34,"Male");
+        mActorList = new ArrayList<>();
+        mActorList.add(johnCena);
+        mActorList.add(chloeBennet);
+        mActorList.add(sethRogen);
+        mRecyclerAdapter = new RecyclerAdapter(mActorList,this);
+        layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setAdapter(mRecyclerAdapter);
+
     }
 }
--- Actors_List_Trial1/app/src/main/java/ly/generalassemb/actors_list_trial1/RecyclerAdapter.java
+++ Actors_List_Trial1/app/src/main/java/ly/generalassemb/actors_list_trial1/RecyclerAdapter.java
@@ -38,6 +38,7 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.myView
 
     @Override
     public void onBindViewHolder(myViewHolder holder, int position) {
+
         holder.actorName.setText(mActorList.get(position).getName());
         holder.actorAge.setText(mActorList.get(position).getAge());
         holder.actorSex.setText(mActorList.get(position).getSex());
